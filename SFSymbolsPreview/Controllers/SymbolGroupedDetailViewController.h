600295d43742df0c27e57f9a02f075353f80f08e
82Flex <82flex@gmail.com>
2022-03-09 15:11:42 +0800

    Add initializer for new detail viewer

--- SFSymbolsPreview/Controllers/SymbolGroupedDetailViewController.h
+++ SFSymbolsPreview/Controllers/SymbolGroupedDetailViewController.h
@@ -6,11 +6,13 @@
 //  Copyright © 2022 YICAI YANG. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "SFSymbol.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface SymbolGroupedDetailViewController : UIViewController
+
+- (instancetype)initWithSymbol:(SFSymbol *)symbol;
 
 @end
 
