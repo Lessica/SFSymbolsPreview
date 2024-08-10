42bc194a1619b3cbf8dfb975a38a3c1435a4c04a
82Flex <82flex@gmail.com>
2024-08-10 23:16:32 +0800

    rename
    Signed-off-by: 82Flex <82flex@gmail.com>

--- DevKit/generate_resources.py
+++ DevKit/generate_resources.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+import os
+import pathlib
+
+# chdir to parent dir
+os.chdir(pathlib.Path(__file__).parent)
+
+# generate resources
+src_app_path = "/Applications/SF Symbols.app/Contents"
+
+# copy version.plist
+src_version_plist_path = f"{src_app_path}/version.plist"
+dst_version_plist_path = f"SFSymbolsPreview/Resources/version.plist"
